17184e8a58cd5c8503b10e3d05ec00b7aa0761ef
iqbalatma <iqbalatma@gmail.com>
2023-07-29 18:34:05 +0700

    add auth service interface

---
+++ app/Services/Auth/AuthService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services\Auth;
+
+use App\Contracts\Interfaces\Services\Auth\AuthServiceInterface;
+
+class AuthService extends \Iqbalatma\LaravelServiceRepo\BaseService implements AuthServiceInterface
+{
+    public function authenticate(array $credentials)
+    {
+
+    }
+}
